64f4e6ff2d585f350e3b5b8aad73ad6a50040b39
David Villeda <42588903+DavidITT@users.noreply.github.com>
2020-04-26 13:42:47 -0500

    Diseño de ganancia

--- src/main/java/jsf/clases/GananciaController.java
+++ src/main/java/jsf/clases/GananciaController.java
@@ -76,17 +76,17 @@ public class GananciaController implements Serializable {
     public String prepareCreate() {
         current = new Ganancia();
         selectedItemIndex = -1;
-        return "Create";
+        return "List";
     }
 
     public String create() {
         try {
             getFacade().create(current);
-            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("GananciaCreated"));
+            JsfUtil.addSuccessMessage("¡Ganancia creada con exito!");
             return prepareCreate();
         } catch (Exception e) {
-            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
-            return null;
+            JsfUtil.addSuccessMessage("¡Lo sentimos la operación no pudo completarse intente mas tarde!");
+            return "List";
         }
     }
 
@@ -99,11 +99,11 @@ public class GananciaController implements Serializable {
     public String update() {
         try {
             getFacade().edit(current);
-            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("GananciaUpdated"));
-            return "View";
+            JsfUtil.addSuccessMessage("¡Ganancia editada con exito!");
+            return "List";
         } catch (Exception e) {
-            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
-            return null;
+            JsfUtil.addSuccessMessage("¡Lo sentimos la operación no pudo completarse intente mas tarde!");
+            return "List";
         }
     }
 
@@ -132,9 +132,10 @@ public class GananciaController implements Serializable {
     private void performDestroy() {
         try {
             getFacade().remove(current);
-            JsfUtil.addSuccessMessage(ResourceBundle.getBundle("/Bundle").getString("GananciaDeleted"));
+            JsfUtil.addSuccessMessage("¡Ganancia eliminada con exito!");
+
         } catch (Exception e) {
-            JsfUtil.addErrorMessage(e, ResourceBundle.getBundle("/Bundle").getString("PersistenceErrorOccured"));
+            JsfUtil.addSuccessMessage("¡Lo sentimos la operación no pudo completarse intente mas tarde!");
         }
     }
 
